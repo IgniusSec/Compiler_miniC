f3678c021fceb00003c0ebabb45d11433dd967bc
Ivan Marcelino <ivanmarcelino2004@gmail.com>
2025-10-08 00:21:01 -0300

    mudando testes

--- teste.c
+++ teste.c
@@ -1,3 +1,4 @@
+int function2(int valor1, char valor2, float valor3[]) { return 0; }
 int main() {
 
   int x;
@@ -8,6 +9,7 @@ int main() {
   c_a = 'A';
   char s[8];
   s = "hello";
+  int i;
 
   if (x >= 10 && y < 4.0) {
     x = x + 1;
@@ -21,10 +23,12 @@ int main() {
     break;
   }
 
-  for (int i = 0; i < 10; i = i + 1) {
+  for (i = 0; i < 10; i = i + 1) {
     x = x + i;
   }
 
   float array[1];
   array[0] = x * 2 % 5 / y;
+
+  function2(x, c_a, s);
 }
---
+++ teste2.c
@@ -0,0 +1,8 @@
+int function2(int a) { return 0; }
+int main() {
+
+  int a;
+  a = 2;
+  char a;
+  function2(a);
+}
